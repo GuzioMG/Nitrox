7495691a3b7f436ffa68327eddd6e95aad3cc1d2
GuzioMG <star.master.123.mg@gmail.com>
2020-01-14 17:55:47 +0200

    Added IP command.
    Right now, it only crashes the server, but at least, it's recognized by command engine.

---
+++ NitroxServer/ConsoleCommands/IpCommand.cs
@@ -0,0 +1,24 @@
+﻿using NitroxModel.DataStructures.Util;
+using NitroxModel.DataStructures.GameLogic;
+using NitroxServer.ConsoleCommands.Abstract;
+
+namespace NitroxServer.ConsoleCommands
+{
+    class IpCommand : Command
+    {
+        public IpCommand() : base("ip", Perms.CONSOLE, "", "Show IPs of the server.", new string[] {"ipconfig", "getip", "whatismyipdotcom"})
+        {
+
+        }
+
+        public override void RunCommand(string[] args, Optional<Player> player)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool VerifyArgs(string[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
