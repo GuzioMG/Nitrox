a5e9efb073a1f4fb26f7d90eef10f49cc0f64f45
GuzioMG <star.master.123.mg@gmail.com>
2020-01-14 22:47:49 +0200

    Implemented ipconfig command.
    It took me a while. ...Actually not. It took me 20 seconds. I swas simply focused on something else  and forgot I have a commit to push.
    
    Can anyone axplain what/who the heck is "the lowliest of cogs"? I've seen this easteregg a few times in this mod, but I have no clue whatsoever, what does it mean.

--- NitroxServer/ConsoleCommands/IpCommand.cs
+++ NitroxServer/ConsoleCommands/IpCommand.cs
@@ -1,24 +1,26 @@
 ﻿using NitroxModel.DataStructures.Util;
 using NitroxModel.DataStructures.GameLogic;
 using NitroxServer.ConsoleCommands.Abstract;
+using static NitroxModel.Logger.Log;
 
 namespace NitroxServer.ConsoleCommands
 {
     class IpCommand : Command
     {
-        public IpCommand() : base("ip", Perms.CONSOLE, "", "Show IPs of the server.", new string[] {"ipconfig", "getip", "whatismyipdotcom"})
+        public IpCommand() : base("ip", Perms.CONSOLE, "", "Show IPs of the server.", new string[] {"ipconfig", "getip", "whatismyip-dot-com", "ips"})
         {
-
         }
 
         public override void RunCommand(string[] args, Optional<Player> player)
         {
-            throw new System.NotImplementedException();
+            Info("Printing IP...");
+            IpLogger.PrintServerIps();
+            Info("IPs printed. If nothing got displayed, wait a few seconds. If it still doesn't seem to work (or thrown an exception), figure IPs yourself. It's not that hard. Even the lowliest of cogs can do it!"); //I have no idea, what/who the heck is "the lowliest of cogs", but I've seen this easteregg couple times in this mod.
         }
 
         public override bool VerifyArgs(string[] args)
         {
-            throw new System.NotImplementedException();
+            return (args.Length == 0);
         }
     }
 }
