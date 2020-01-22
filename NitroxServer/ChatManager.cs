02a2366602b4d2b758b51d3e551fc36464cef517
GuzioMG <star.master.123.mg@gmail.com>
2020-01-22 19:18:46 +0200

    Extended ChatManager functionality
    I know it's a bit off-topic for a cleaning-the-mess branch, but no one will see that on official repo's commit history anyway. May be a bit of a problem during pull request evaluation, but I don't care.
    
    * Added a regular message sender, so it's possible to send message without Player being an Optional<>. 2 forms avaible: with builder and without.
    
    * Added fake message sender, , so it's possible to send message as someone
    
    * Added message builder

--- NitroxServer/ChatManager.cs
+++ NitroxServer/ChatManager.cs
@@ -5,12 +5,47 @@ namespace NitroxServer
 {
     class ChatManager
     {
-        public static void SendServerMessageIfPlayerIsPresent(Optional<Player> player, string message)
+        public static void        SendPrebuildMessage(ChatMessage chatMessage, Player target)
         {
+            target.SendPacket(chatMessage);
+        }
+
+        public static ChatMessage SendServerMessage  (Player player, string message)
+        {
+            ChatMessage chatMessage = messageBuilder(message, 0, true);
+            SendPrebuildMessage(chatMessage, player);
+            return chatMessage;
+        }
+
+        public static ChatMessage SendFakeMessage   (Player player, string message, ushort fakePlayerID)
+        {
+            ChatMessage chatMessage = messageBuilder(message, fakePlayerID, false);
+            SendPrebuildMessage(chatMessage, player);
+            return chatMessage;
+        }
+
+
+
+        public static ChatMessage SendServerMessageIfPlayerIsPresent(Optional<Player> player, string message)
+        {
+            ChatMessage chatMessage = null;
             if (player.IsPresent())
             {
-                player.Get().SendPacket(new ChatMessage(ChatMessage.SERVER_ID, message));
+                chatMessage = SendServerMessage(player.Get(), message);
             }
+            return chatMessage;
+        }
+
+
+
+        public static ChatMessage messageBuilder(string message, ushort sender, bool asServer)
+        {
+            ChatMessage chatMessage;
+            if(asServer){
+                sender = ChatMessage.SERVER_ID;
+            }
+            chatMessage = new ChatMessage(sender, message);
+            return chatMessage;
         }
     }
 }
